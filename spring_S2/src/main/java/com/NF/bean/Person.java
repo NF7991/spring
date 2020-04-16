06de33ae0ad2221b17dc1cb189f0f711f3394429
NF7991 <627084296@qq.com>
2020-04-16 17:33:11 +0800

    Spring2
    Spring2_0

---
+++ spring_S2/src/main/java/com/NF/bean/Person.java
@@ -0,0 +1,140 @@
+package com.NF.bean;
+
+
+import java.util.*;
+
+public class Person {
+    private int id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private String[] hobbios;
+    private Address address;
+
+    private List<Address> lists;
+    private Set<String> sets;
+    private Map<String,Object> maps;
+    private Properties properties;
+
+
+    //无参构造
+    public Person(){
+        System.out.println("person被创建");
+    }
+    //有参构造
+    public Person(int id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public Person(int id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        System.out.println("age...");
+
+    }
+    public Person(int id, String name, String gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        System.out.println("gendre...");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String[] getHobbios() {
+        return hobbios;
+    }
+
+    public void setHobbios(String[] hobbios) {
+        this.hobbios = hobbios;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Address> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Address> lists) {
+        this.lists = lists;
+    }
+
+    public Set<String> getSets() {
+        return sets;
+    }
+
+    public void setSets(Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", hobbios=" + Arrays.toString(hobbios) +
+                ", address=" + address +
+                ", lists=" + lists +
+                ", sets=" + sets +
+                ", maps=" + maps +
+                ", properties=" + properties +
+                '}';
+    }
+}
---
+++ spring_S2/src/test/java/myTest.java
@@ -0,0 +1,55 @@
+import com.NF.bean.Address;
+import com.NF.bean.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class myTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
+        /**
+        * 根据bean标签来获取对象
+        * */
+ //       Person person = context.getBean("person",Person.class);
+//        Person person2 = context.getBean("person",Person.class);
+//        System.out.println(person);
+//        //单例模式 结果true
+//        System.out.println(person == person2);
+
+        /**
+         * 根据bean的类型来获取对象
+         * 配置文件中有多个相同类型的对象，将无法完成工作 注意注意！
+         */
+//        Person bean = context.getBean(Person.class);
+//        System.out.println(bean);
+
+        /**
+         * 当需要从容器中获取对象的时候，最好要保留无参构造方法，因为底层的实现是反射
+         *  Class clazz = Person.class;
+         *  Object obj = clazz.newInstance();默认调用无参的构造方法，此方法已经被弃用
+         *  Object obj = class.getDeclaredConstructor().newInstance()
+         */
+
+
+//        Person person2 = context.getBean("person2", Person.class);
+//        System.out.println(person2);
+//
+//        Person person3 = context.getBean("person3", Person.class);
+//        System.out.println(person2);
+
+//        Person person = context.getBean("person", Person.class);
+//        System.out.println(person);
+//
+//        Person person5 = context.getBean("person5", Person.class);
+//        System.out.println(person5);
+
+//        Person person6 = context.getBean("person6", Person.class);
+//        System.out.println(person6);
+
+//        Address address2 = context.getBean("address2", Address.class);
+//        System.out.println(address2);
+        Person son = context.getBean("son", Person.class);
+        System.out.println(son);
+        Person parent = context.getBean("parent", Person.class);
+        System.out.println(parent);
+    }
+}
